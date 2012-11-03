aefd72d862ade829adba4b416e7b3621f0d70026
Kri <newvgund@gmail.com>
2012-11-03 19:47:52 +0900

    실행 파일 출력을 위한 main문
    cx_freeze 로 실행파일 작성, 주의 사항은 아래 참고
    
    * Python 3.3 에서는 제대로 실행파일이 실행되지 않는다. 그 이하의 버전으로 할 것.

--- src/python/console_main.py
+++ src/python/console_main.py
@@ -3,49 +3,53 @@ from utils import *
 
 from data import *
 
-# 메인 루프를 돌며, 플레이어의 입력을 기다린다.
-while 1:
-    # 기본적인 맵을 그린다.
-    """
-    for layer in fieldMap:
-        for row in layer:
-            for col in row:
-                col.Draw()
-            print()
-    """
-    r = 0
-    c = 0
-
+def main():
+    # 메인 루프를 돌며, 플레이어의 입력을 기다린다.
     while 1:
-        print('[', end='')
-        for i in range(3):
-            fieldMap[i][c][r].Draw()
-        print(']', end='')
+        # 기본적인 맵을 그린다.
+        """
+        for layer in fieldMap:
+            for row in layer:
+                for col in row:
+                    col.Draw()
+                print()
+        """
+        r = 0
+        c = 0
 
-        if r < row - 1:
-            r = r + 1
-        else:
-            print()
-            r = 0
-            c = c + 1
+        while 1:
+            print('[', end='')
+            for i in range(3):
+                fieldMap[i][c][r].Draw()
+            print(']', end='')
 
-            if c >= col:
-                break
+            if r < row - 1:
+                r = r + 1
+            else:
+                print()
+                r = 0
+                c = c + 1
 
-    # 사용자에게 입력을 받는다.
-    userCommand = input()
+                if c >= col:
+                    break
 
-    MovingCommand = ['w', 's', 'a', 'd']
-    if not userCommand in MovingCommand:
-        continue
+        # 사용자에게 입력을 받는다.
+        userCommand = input()
 
-    if userCommand == 'w':
-        userCommand = "UP"
-    if userCommand == 's':
-        userCommand = "DOWN"
-    if userCommand == 'a':
-        userCommand = "LEFT"
-    if userCommand == 'd':
-        userCommand = "RIGHT"
+        MovingCommand = ['w', 's', 'a', 'd']
+        if not userCommand in MovingCommand:
+            continue
 
-    MoveCharacter(player, fieldMap, row, col, userCommand)
+        if userCommand == 'w':
+            userCommand = "UP"
+        if userCommand == 's':
+            userCommand = "DOWN"
+        if userCommand == 'a':
+            userCommand = "LEFT"
+        if userCommand == 'd':
+            userCommand = "RIGHT"
+
+        MoveCharacter(player, fieldMap, row, col, userCommand)
+
+if __name__ == "__main__":
+    main()
--- src/python/pygame_main.py
+++ src/python/pygame_main.py
@@ -6,19 +6,9 @@ from data import *
 import sys
 import pygame
 
-# 파이 게임을 초기화 한다.
-pygame.init()
-
-window = pygame.display.set_mode((800, 600)) 
-pygame.display.set_caption("Roguelike")
-screen = pygame.display.get_surface()
-
 # 명령 리스트
 orderList = []
 
-# ResourceManager의 파일들을 갱신해 두자.
-ResourceManager.Get().LoadAndReflushImage(pygame.image.load)
- 
 def KeyInput(events): 
    for event in events: 
       if event.type == pygame.QUIT: 
@@ -47,53 +37,70 @@ def KeyInput(events):
                global orderList
                orderList = PathFindAndMove(player, fieldMap, 5, 5)
             
-while True:
-   # 가장 먼저 플레이어가 행동 하고,
-   KeyInput(pygame.event.get())
 
-   # 그 뒤에 Nonplayer 들이 행동을 한다.
-   #
+def main():
+   # 파이 게임을 초기화 한다.
+   pygame.init()
 
-   # 모든 사이클이 끝났다면, 이제 장면을 렌더링.
+   window = pygame.display.set_mode((800, 600)) 
+   pygame.display.set_caption("Roguelike")
+   screen = pygame.display.get_surface()
 
-   #
-   screen.fill((0, 0, 0))
+   # ResourceManager의 파일들을 갱신해 두자.
+   ResourceManager.Get().LoadAndReflushImage(pygame.image.load)
 
-   # 장면 렌더링.
-   for i in range(3):
-      for x in range(row):
-         for y in range(col):
-            image = ResourceManager.Get().GetLoadedResource(fieldMap[i][x][y].GetProperty("ResourceID"))
-            if image is not None:
-               screen.blit(image, (x * 48, y * 48))
+   global orderList
 
-   if len(orderList) > 0:
-      print("Order Size : ", len(orderList))
-      order = orderList[0]
-      orderList.remove(order)
-      playerPos = player.GetY() * 10 + player.GetX()
+   while True:
+      # 가장 먼저 플레이어가 행동 하고,
+      KeyInput(pygame.event.get())
 
-      if playerPos > order:
-         # playerPos 가 order 보다 크다.
-         if playerPos - 10 == order:
-            print("Do Down!")
-            MoveCharacter(player, fieldMap, row, col, "UP")
-         elif playerPos - 1 == order:
-            print("Do Right!")
-            MoveCharacter(player, fieldMap, row, col, "LEFT")
-      else:
-         # playerPos 가 order 보다 작다.
-         if playerPos + 10 == order:
-            print("Do Up!")
-            MoveCharacter(player, fieldMap, row, col, "DOWN")
-         elif playerPos + 1 == order:
-            print("Do Left!")
-            MoveCharacter(player, fieldMap, row, col, "RIGHT")
+      # 그 뒤에 Nonplayer 들이 행동을 한다.
+      #
+
+      # 모든 사이클이 끝났다면, 이제 장면을 렌더링.
+
+      #
+      screen.fill((0, 0, 0))
+
+      # 장면 렌더링.
+      for i in range(3):
+         for x in range(row):
+            for y in range(col):
+               image = ResourceManager.Get().GetLoadedResource(fieldMap[i][x][y].GetProperty("ResourceID"))
+               if image is not None:
+                  screen.blit(image, (x * 48, y * 48))
+
+      if len(orderList) > 0:
+         print("Order Size : ", len(orderList))
+         order = orderList[0]
+         orderList.remove(order)
+         playerPos = player.GetY() * 10 + player.GetX()
+
+         if playerPos > order:
+            # playerPos 가 order 보다 크다.
+            if playerPos - 10 == order:
+               print("Do Down!")
+               MoveCharacter(player, fieldMap, row, col, "UP")
+            elif playerPos - 1 == order:
+               print("Do Right!")
+               MoveCharacter(player, fieldMap, row, col, "LEFT")
+         else:
+            # playerPos 가 order 보다 작다.
+            if playerPos + 10 == order:
+               print("Do Up!")
+               MoveCharacter(player, fieldMap, row, col, "DOWN")
+            elif playerPos + 1 == order:
+               print("Do Left!")
+               MoveCharacter(player, fieldMap, row, col, "RIGHT")
+            
          
-      
 
-   #playerImg = ResourceManager.Get().GetLoadedResource(player.GetProperty("ResourceID"))
-   #if playerImg is not None:
-    #  screen.blit(playerImg, (player.GetX() * 48, player.GetY() * 48))
+      #playerImg = ResourceManager.Get().GetLoadedResource(player.GetProperty("ResourceID"))
+      #if playerImg is not None:
+       #  screen.blit(playerImg, (player.GetX() * 48, player.GetY() * 48))
 
-   pygame.display.flip()
+      pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
